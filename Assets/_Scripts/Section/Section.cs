65794cdd667eef8acc7898a6af4fa0f493b700b4
JackB <volcanicpiggames@gmail.com>
2022-01-20 20:41:57 +0000

    Startting codie stuffs

---
+++ Assets/_Scripts/Section/Section.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Section : MonoBehaviour
+    {
+        private List<Enemy> _enemies = new List<Enemy>(); 
+        
+        
+    }
+}
