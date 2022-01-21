569b8847851dea883cea1e2e3e534aeb08f69cc1
JackB <volcanicpiggames@gmail.com>
2022-01-21 00:10:30 +0000

    Epic Toon FX added, added an enemy to bully

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,32 @@ namespace Game
 {
     public class Enemy : MonoBehaviour
     {
+        [SerializeField] private GameObject deathParticle;
 
+        private Health _health;
+
+        private void Awake()
+        {
+            _health = GetComponent<Health>();
+            _health.Killed += OnDeath; 
+        }
+
+        private void OnDisable()
+        {
+            _health.Killed -= OnDeath; 
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                _health.TakeHealth(1);
+            }
+        }
+
+        private void OnDeath()
+        {
+            Instantiate(deathParticle, transform.position, Quaternion.identity); 
+        }
     }
 }
